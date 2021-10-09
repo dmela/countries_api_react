5cd449487703dab4764470080d022a44a173b261
dmeland <daniel.melander@luke.fi>
2021-10-09 12:44:50 +0300

    feat: construct the data object for the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import temp_flag from './resources/example_flag.png'
+// let temp_flag = require('./resources/example_flag.png') // toimii myös, mutta temp_flag.default
 
+let json_obj = require('./resources/example_data.json')
 
 function App() {
+
+  console.log(json_obj)
+  const country_name = json_obj[0].name.official
+  const capital = json_obj[0].capital[0]
+  const region = json_obj[0].region
+  const languages = json_obj[0].languages
+  const flag = temp_flag
+
+  const data_obj = {
+    "country_name": country_name,
+    "capital": capital,
+    "region": region,
+    "languages": languages,
+    "flag": flag
+  } 
+
+
   return (
     <div className="Countries_app">
     </div>
